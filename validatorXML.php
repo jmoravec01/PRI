db30c579101b714416b5f86fcf181fa830532a26
jmoravec01 <kubik.moravec@email.cz>
2023-06-07 17:34:37 +0200

    oujes Validátor DONE

--- validatorXML.php
+++ validatorXML.php
@@ -135,34 +135,46 @@
         });
     </script>
     <script>
-        var fileDrop = document.getElementById('file-drop');
-        var fileInput = document.getElementById('xmlFile');
-
-        fileDrop.addEventListener('dragover', function(event) {
-            event.preventDefault();
-            fileDrop.classList.add('drag-over');
-        });
-
-        fileDrop.addEventListener('dragleave', function() {
-            fileDrop.classList.remove('drag-over');
-        });
-
-        fileDrop.addEventListener('drop', function(event) {
-            event.preventDefault();
-            fileDrop.classList.remove('drag-over');
-
-            var files = event.dataTransfer.files;
-            fileInput.files = files;
-        });
-
-        fileInput.addEventListener('change', function() {
-            fileDrop.classList.remove('drag-over');
-        });
-    </script>
-    <script>
         var fileInput = document.getElementById("xmlFile");
         var dragText = document.querySelector(".drag-text");
 
+        fileInput.addEventListener("change", function() {
+            if (fileInput.files.length > 0) {
+                dragText.textContent = fileInput.files[0].name;
+            } else {
+                dragText.textContent = "Přetáhněte soubor sem";
+            }
+        });
+    </script>
+    <script>
+        var fileDrop = document.getElementById("file-drop");
+        var fileInput = document.getElementById("xmlFile");
+        var dragText = document.querySelector(".drag-text");
+
+        // Handle drag events
+        fileDrop.addEventListener("dragover", function(e) {
+            e.preventDefault();
+            fileDrop.classList.add("hover");
+            dragText.textContent = "Pusť";
+        });
+
+        fileDrop.addEventListener("dragleave", function(e) {
+            e.preventDefault();
+            fileDrop.classList.remove("hover");
+            dragText.textContent = "Přetáhněte soubor sem";
+        });
+
+        fileDrop.addEventListener("drop", function(e) {
+            e.preventDefault();
+            fileDrop.classList.remove("hover");
+            var files = e.dataTransfer.files;
+            if (files.length > 0) {
+                fileInput.files = files;
+                dragText.textContent = files[0].name;
+            }
+        });
+
+        // Handle file input change event
         fileInput.addEventListener("change", function() {
             if (fileInput.files.length > 0) {
                 dragText.textContent = fileInput.files[0].name;
